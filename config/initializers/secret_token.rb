8010cc685d9d4ca5680d3ab336ef9b5f5ece5c31
Jasonvdm <jason.merwe@gmail.com>
2012-12-12 16:16:39 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collegefitness::Application.config.secret_token = '755302a3ea571eabb81af9df4e4deed1276f59bf1715ccf81710d7bba9e5fcd7c896a1a7c1c0f8ed7749e0a6b86c1c29769d6ea3f763561c105814419d73a51e'
