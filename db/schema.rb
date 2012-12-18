75d6717734252e2e3a3ec81ad0f308332239fecd
Jasonvdm <jason.merwe@gmail.com>
2012-12-18 17:09:11 -0500

    added new background, formatted user show and edit pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121216205116) do
+ActiveRecord::Schema.define(:version => 20121218202014) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -31,5 +31,15 @@ ActiveRecord::Schema.define(:version => 20121216205116) do
   end
 
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "workouts", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "difficulty"
+    t.boolean  "equipment"
+    t.string   "equipmentneeded"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
 end
