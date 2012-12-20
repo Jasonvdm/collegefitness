1974d8674ef892dfb6f61350bf75d17c809130da
David Khavari <dkhavari@gmail.com>
2012-12-20 14:07:18 -0800

    Fixed the workout picture sizing.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,5 @@
 class Workout < ActiveRecord::Base
   attr_accessible :description, :difficulty, :equipment, :equipmentneeded, :name
   attr_accessible :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :avatar, :styles => { :medium => "232x155#", :thumb => "100x100>", :large => "408x272>" }
 end
