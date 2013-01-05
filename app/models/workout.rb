4222d082a00b3d7df8b3ac3ceb30945e81b30525
Jasonvdm <jason.merwe@gmail.com>
2013-01-04 23:29:23 -0500

    fixed some CSS on index for workouts

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,6 @@
 class Workout < ActiveRecord::Base
 	
   attr_accessible :description, :difficulty, :equipment, :equipmentneeded, :name
-  attr_accessible :avatar, :has_equipment, :equipment_list
+  attr_accessible :avatar, :has_equipment, :equipment_list, :workout_type
   has_attached_file :avatar, :styles => { :medium => "232x155#", :thumb => "100x100>", :large => "408x272>" }
 end
