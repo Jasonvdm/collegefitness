a9b2b5f9eb7577e531667b514bc1c9e6d02ed1c7
Jasonvdm <jason.merwe@gmail.com>
2013-01-09 23:55:07 -0800

    added google analytics to pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,4 +37,22 @@ module ApplicationHelper
       'Strength'
     ]
   end
+
+
+  def google_analytics
+    return if request.local?
+    analytics = <<-EOF
+      <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-23113828-4']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+      </script>
+    EOF
+    analytics.html_safe
+  end
 end
